d5bae566c8e814e8de06c1a30901022287ed4be4
andrew lupton <aflupton@gmail.com>
2018-06-27 11:15:02 -0700

    add dev environment changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import Header from "./Header";
-import TicketList from "./TicketList";
+import React from 'react';
+import Header from './Header';
+import TicketList from './TicketList';
 
 function App(){
   var myStyledComponentStyles = {
     backgroundColor: '#ecf0f1',
     fontFamily: 'sans-serif',
     paddingTop: '50px'
-  }
+  };
   return (
     <div style={myStyledComponentStyles}>
       <Header/>
