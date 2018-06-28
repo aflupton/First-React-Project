c9ce091a81b8b3f2c0855697c312efc71a2f99c2
andrew lupton <aflupton@gmail.com>
2018-06-28 14:16:55 -0700

    add error message

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import queue from '../assets/images/queue.jpg';
 
 function Error404(props){
   console.log(props);
   return (
     <div>
+      <hr/>
+      <img className="image" src={queue} />
+      <hr/>
       <h2>The page you are looking for does not exist!</h2>
       <h3>Would you like to return <Link to="/">home</Link> instead?</h3>
+
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import queue from '../assets/images/queue.jpg';
 
 function Header() {
   return (
     <div>
-      <style jsx>{`
+      <style global jsx>{`
         .header {
           color: #fff;
           background-color: grey;
@@ -18,8 +17,6 @@ function Header() {
       `}</style>
       <h1 className="header">Help Queue:</h1>
       <Link to="/">Home</Link> | <Link to="/newticket">Create Ticket</Link>
-      <hr/>
-      <img className="image" src={queue}/>
     </div>
   );
 }
