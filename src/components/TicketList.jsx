ddcbc3d436cb6b0ae61852f59de41c9f06a2b1d1
andrew lupton <aflupton@gmail.com>
2018-07-03 16:43:40 -0700

    add linter rules and unique ids

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -4,15 +4,16 @@ import Ticket from './Ticket';
 
 
 function TicketList(props){
+  console.log(props.ticketList);
   return (
     <div>
       <hr/>
-      {props.ticketList.map((ticket, index) =>
+      {props.ticketList.map((ticket) =>
         <Ticket
           names={ticket.names}
           location={ticket.location}
           issue={ticket.issue}
-          key={index}/>
+          key={ticket.id}/>
       )}
     </div>
   );
